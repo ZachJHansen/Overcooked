41b1713a9c79aaeac77e9e978491331dc77dd063
Zachansen <zachary.hansen@ttu.edu>
2020-03-24 15:22:47 -0500

    Added update leftover score method to Recipe class

--- Recipe.py
+++ Recipe.py
@@ -1,4 +1,6 @@
-from bitarray import bitarray                                       # pip install bitarray
+from bitarray import bitarray   
+import json
+from jsmin import jsmin                                     # pip install bitarray
 
 # Globals
 COOKWARE_LENGTH = 100
@@ -25,23 +27,51 @@ class Recipe:
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def update_leftover_score(self, ingredients, weights, units):
-        print("Stub function")
+    # Restrict ourselves to primary ingredients for now
+    # MDU = minimum discrete unit purchasable at store (D)
+    # S = minimum natural number st S * MDU > Quanitity Required
+    def update_leftover_score(self, table_data):
+        leftovers = []
+        for ingr_table in table_data:
+            S = 1
+            MDU = ingr_table['MDU']
+            ingr_index = self.primary_ingredients.index(ingr_table['id'])
+            required = self.primary_quantities[ingr_index]
+            while (S * MDU < required):     
+                S += 1
+            leftover = (S * MDU - required) * ingr_table['weight']
+            leftovers.append(leftover)
+        self.leftover_score = sum(leftovers)
 
     def update_buddies(self):
         print("Stub function")
         
 if __name__=="__main__":
-    ingredients = tuple((["rice", "beans"], ["red pepper"]))
-    additional_info = {
-        "restrictions": ["vegan", "gluten free"],
-        "time": 45,
-        "complexity": "hard",
-        "new_arg": "This wasn't in the original constructor"
-    }
-    r1 = Recipe("Spaghetti-os", ingredients)
-    print(r1.name)
-    r1.update_tags(**additional_info)
-    print(r1.link)
-    print(r1.restrictions)
-    print("New Member:", r1.new_arg)
+    if False:
+        ingredients = tuple((["rice", "beans"], ["red pepper"]))
+        additional_info = {
+            "restrictions": ["vegan", "gluten free"],
+            "time": 45,
+            "complexity": "hard",
+            "new_arg": "This wasn't in the original constructor"
+        }
+        r1 = Recipe("Spaghetti-os", ingredients)
+        print(r1.name)
+        r1.update_tags(**additional_info)
+        print(r1.link)
+        print(r1.restrictions)
+        print("New Member:", r1.new_arg)
+    with open("tables.jsonc") as tables:
+        with open("recipeV1.jsonc") as recipe_file:
+            recipe_records = json.loads(jsmin(recipe_file.read()))
+            prim_ingr_table = json.loads(jsmin(tables.read()))["Primary_ingredients"]
+            for recipe in recipe_records:
+                name = recipe["name"]
+                ingredients = tuple((recipe["primary_ingredients"], recipe["secondary_ingredients"]))
+                quantities = tuple((recipe["primary_quanitites"], recipe["secondary_quantities"]))
+                table_data = []
+                for pi in ingredients[0]:
+                    table_data.append(prim_ingr_table[pi])
+                r = Recipe(name, ingredients, quantities)
+                r.update_leftover_score(table_data)
+                print("Leftover score", r.leftover_score)
