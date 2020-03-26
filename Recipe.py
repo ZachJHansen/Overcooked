e7ef57c2f908e352f527f7c5a7407b3ff7ea36a9
Zachansen <zachary.hansen@ttu.edu>
2020-03-26 13:20:16 -0500

    Added buddy score calculations

--- Recipe.py
+++ Recipe.py
@@ -1,17 +1,13 @@
 from bitarray import bitarray   
-import json
-from jsmin import jsmin                                     # pip install bitarray
 
 # Globals
 COOKWARE_LENGTH = 100
 
 class Recipe:
-    def __init__(self, name, ingredients, quantities, **kwargs):    # Not all recipes will have information such as complexity and calorie count
+    def __init__(self, name, ingredients, **kwargs):                # Not all recipes will have information such as complexity and calorie count
         self.name = name                                            # Hence, None initialization or empty arrays
-        self.primary_ingredients = ingredients[0]                   # Minimum of recipe name, quantities, and ingredients (tuple of arrays) must be provided
+        self.primary_ingredients = ingredients[0]                   # Minimum of recipe name, quantities, and ingredients must be provided
         self.secondary_ingredients = ingredients[1]
-        self.primary_quantities = quantities[0]
-        self.secondary_quantities = quantities[1]
         self.allergens = kwargs.get('allergens', [])
         self.restrictions = kwargs.get('restrictions', [])
         self.complexity = kwargs.get('complexity', None)
@@ -19,8 +15,7 @@ class Recipe:
         self.calories = kwargs.get('calories', None)
         self.link = kwargs.get('link', None)
         self.leftover_score = kwargs.get('leftover_score', 0)
-        self.buddies = kwargs.get('buddies', [])
-        self.buddy_scores = kwargs.get('buddy_scores', [])
+        self.buddies = kwargs.get('buddies', {})
         self.cookware = kwargs.get('cookware', bitarray(COOKWARE_LENGTH))
 
     def update_tags(self, **kwargs):                                # Update any of the standard members above, or add new members with kwargs dict
@@ -29,23 +24,58 @@ class Recipe:
 
     # Restrict ourselves to primary ingredients for now
     # MDU = minimum discrete unit purchasable at store (D)
-    # S = minimum natural number st S * MDU > Quanitity Required
+    # S = minimum natural number st S * MDU > Quanitity Required (QR)
     def update_leftover_score(self, table_data):
         leftovers = []
         for ingr_table in table_data:
-            S = 1
-            MDU = ingr_table['MDU']
-            ingr_index = self.primary_ingredients.index(ingr_table['id'])
-            required = self.primary_quantities[ingr_index]
-            while (S * MDU < required):     
-                S += 1
-            leftover = (S * MDU - required) * ingr_table['weight']
-            leftovers.append(leftover)
+            QR = self.primary_ingredients[ingr_table['id']]
+            leftovers.append(ingredient_score(ingr_table, QR))
         self.leftover_score = sum(leftovers)
 
-    def update_buddies(self):
-        print("Stub function")
-        
+    # Parameters: All recipes except recipe in question, lookup table for primary ingredients
+    # Generate the leftover score of every recipe when combined with the recipe in question
+    # Updates an array of the 3 recipes that pair best with given recipe (lowest leftover score), tuples (id, score)
+    def update_buddies(self, n_minus_1, prim_ingr_table):
+        buddies = [(0,float("inf")), (0,float("inf")), (0,float("inf"))]
+        for candidate in n_minus_1:
+            table_data = []
+            combined_ingredients = set([*(self.primary_ingredients)] + [*(candidate["primary_ingredients"])])       # unpack keys into list literals and combine
+            for pi in combined_ingredients:
+                table_data.append(prim_ingr_table[pi])
+            leftovers = []
+            for ingr_table in table_data:
+                _id = ingr_table['id']
+                if _id in self.primary_ingredients:
+                    QR_1 = self.primary_ingredients[_id]
+                else:
+                    QR_1 = 0
+                if _id in candidate["primary_ingredients"]:
+                    QR_2 = candidate["primary_ingredients"][_id]
+                else:
+                    QR_2 = 0
+                leftovers.append(ingredient_score(ingr_table, QR_1+QR_2))
+            score = sum(leftovers)
+            curr_max_index = buddies[0][0]
+            curr_max_score = buddies[0][1]
+            for i, bud in enumerate(buddies):
+                bID, bScore = bud[0], bud[1]
+                if bScore > curr_max_score:
+                    curr_max_score = bScore
+                    curr_max_index = i
+            if curr_max_score > score:
+                del buddies[curr_max_index]
+                buddies.append(tuple((candidate["rID"], score)))
+        for b in buddies:
+            self.buddies[b[0]] = b[1]
+
+def ingredient_score(ingr_table, QR):
+    S = 1
+    MDU = ingr_table['MDU']
+    while (S * MDU < QR):     
+        S += 1
+    leftover = (S * MDU - QR) * ingr_table['weight']
+    return(leftover)
+          
 if __name__=="__main__":
     if False:
         ingredients = tuple((["rice", "beans"], ["red pepper"]))
@@ -61,17 +91,5 @@ if __name__=="__main__":
         print(r1.link)
         print(r1.restrictions)
         print("New Member:", r1.new_arg)
-    with open("tables.jsonc") as tables:
-        with open("recipeV1.jsonc") as recipe_file:
-            recipe_records = json.loads(jsmin(recipe_file.read()))
-            prim_ingr_table = json.loads(jsmin(tables.read()))["Primary_ingredients"]
-            for recipe in recipe_records:
-                name = recipe["name"]
-                ingredients = tuple((recipe["primary_ingredients"], recipe["secondary_ingredients"]))
-                quantities = tuple((recipe["primary_quanitites"], recipe["secondary_quantities"]))
-                table_data = []
-                for pi in ingredients[0]:
-                    table_data.append(prim_ingr_table[pi])
-                r = Recipe(name, ingredients, quantities)
-                r.update_leftover_score(table_data)
-                print("Leftover score", r.leftover_score)
+
+        # we could have a user-improvement feature where they enter the smallest unit they saw at the store for a certain product
--- parser.py
+++ parser.py
@@ -2,18 +2,60 @@ import json
 from Recipe import Recipe
 from jsmin import jsmin                                     # pip install jsmin
 
-if __name__=="__main__":
+def instantiate_recipe(record, prim_ingr_table):
+    name = record["name"]
+    ingredients = tuple((record["primary_ingredients"], record["secondary_ingredients"]))
+    table_data = []
+    for pi in ingredients[0].keys():
+        table_data.append(prim_ingr_table[pi])
+    r = Recipe(name, ingredients)
+    return(r, table_data)
+
+# Newly added recipes stored in a JSON file at PATH must have the leftover scores added
+# File at PATH will be overwritten with updated values
+def update_lo_scores(path):
     with open("tables.jsonc") as tables:
-        with open("recipeV1.jsonc") as recipe_file:
+        with open(path) as recipe_file:
             recipe_records = json.loads(jsmin(recipe_file.read()))
-            prim_ingr_table = json.loads(jsmin(tables.read()))["Primary_ingredients"]
-            for recipe in recipe_records:
-                name = recipe["name"]
-                ingredients = tuple((recipe["primary_ingredients"], recipe["secondary_ingredients"]))
-                quantities = tuple((recipe["primary_quanitites"], recipe["secondary_quantities"]))
-                table_data = []
-                for pi in ingredients[0]:
-                    table_data.append(prim_ingr_table[pi])
-                r = Recipe(name, ingredients, quantities)
-                r.update_leftover_score(table_data)
-                print("Leftover score", r.leftover_score)
+            prim_ingr_table = json.loads(jsmin(tables.read()))["primary_ingredients"]
+    for recipe in recipe_records:
+        if (recipe["leftover_score"] is None):
+            r, table_data = instantiate_recipe(recipe, prim_ingr_table)
+            r.update_leftover_score(table_data)
+            recipe["leftover_score"] = r.leftover_score
+            print("Leftover score", r.leftover_score)
+    open(path, "w").write(json.dumps(recipe_records))
+
+# When new recipes are added, the entire database's buddies must be updated
+# (A new best bud could be found in the new recipes)        
+# But maybe to reduce complexity we are only interested in overlapping recipes that share > 0 primary ingredients
+def update_new_buddies(path):
+    ingredients = set()                                           # Union of all primary ingredients required by new recipes
+    with open("tables.jsonc") as tables:
+        with open(path) as recipe_file:
+            new_recipes = json.loads(jsmin(recipe_file.read()))
+            prim_ingr_table = json.loads(jsmin(tables.read()))["primary_ingredients"]
+    for recipe in new_recipes:
+        for ingr in recipe["primary_ingredients"].keys():
+            ingredients.add(ingr)
+    old_recipes = read_from_db(ingredients)                         # Only compare recipes that share primary ingredients with new recipes
+    #recipes = old_recipes + new_recipes                         # Shallow copies, no memory issue, all new + fetched recipes will be updated
+    recipes = new_recipes                                           # REMOVE EVENTUALLY
+    for recipe in recipes:
+        r, table_data = instantiate_recipe(recipe, prim_ingr_table)
+        n_minus_1 = [r for r in recipes if r != recipe]
+        r.update_buddies(n_minus_1, prim_ingr_table)
+        print("Buddies")
+        print(r.buddies)
+        recipe["buddies"] = r.buddies
+    open("output.jsonc", "w").write(json.dumps(recipes))                  # Overwrite file with updated recipes from entire DB + new recipes
+
+def read_from_db(ingredients):
+    # fetch all recipes from db that contain at least one of the specified ingredients
+    # return a list of dicts
+    return(["stub function"])
+
+
+if __name__=="__main__":
+    update_lo_scores("master_recipes.jsonc")
+    update_new_buddies("master_recipes.jsonc")
