c0d4106447155d3f52b27bdb139faef9e6967145
gsmbaku <gsmbaku1@gmail.com>
2020-05-04 03:39:48 -0500

    Add files via upload

--- overcooked_app/MainActivity.java
+++ overcooked_app/MainActivity.java
@@ -5,26 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity_Find_Data();
+            }
+        });
+
     }
 
-
-
-    public void findRecipe (View view)
-    {
-
-        startActivity(new Intent(this,FindData.class));
+    public void openActivity_Find_Data(){
+        Intent intent = new Intent (this, FindData.class);
+        startActivity(intent);
+    }
     }
 
-    public void shoppingList (View view)
-    {
-        startActivity(new Intent(this,ShoppingList.class));
-    }
-
-}
