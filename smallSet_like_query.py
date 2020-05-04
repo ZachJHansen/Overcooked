54acef61b1de5076f5ca8587796744c0e6af5f49
cballew96 <38291954+cballew96@users.noreply.github.com>
2020-05-03 21:01:04 -0500

    Retrieve recipe from database

---
+++ smallSet_like_query.py
@@ -0,0 +1,39 @@
+#works with sqliteRecipeList.db
+import flask
+from flask import request, jsonify
+import sqlite3
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+#Grabs the main_ingredient parameter off the url
+@app.route('/', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+    
+    main_ingredient = query_parameters.get('main_ingredient')
+    
+    to_filter = []
+
+    if main_ingredient:
+      to_filter.append("%" + main_ingredient + "%")	
+
+	#connection to database sqliteRecipeList.db 
+    conn = sqlite3.connect('sqliteRecipeList.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    
+	#Searches database for string that matches main_ingredient from the url
+    print(to_filter)
+    results = cur.execute("SELECT title, ingredients, instructions FROM smallSet WHERE title LIKE ?", to_filter).fetchall()
+
+    return jsonify(results)
+
+  
+app.run()
